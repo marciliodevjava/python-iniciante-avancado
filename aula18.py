5dc92d27550658df376fd265fda53b31ca39155e
marcsilva <marciliodevjava@gmail.com>
2024-01-18 17:22:15 -0300

    aula 18

---
+++ aula18.py
@@ -0,0 +1,23 @@
+a = 2
+b = 3
+
+print('A:', a)
+print('B:', b)
+print(f"{a} + {b} = {a + b}")
+
+d = 2.0
+e = 10
+
+print('D:', d)
+print('E:', e)
+print(f'{d} + {e} = {d + e}')
+
+palavra = 'Sou uma String'
+verdadeiro_ou_falso = True
+valor = 10
+numero_flutuante = 2.0
+
+print('Palavra: {}'.format(palavra))
+print('Verdadeiro ou Falso: {}'.format(verdadeiro_ou_falso))
+print('Número inteiro: {}'.format(valor))
+print('Número Flutuante: {}'.format(numero_flutuante))
