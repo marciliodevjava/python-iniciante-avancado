d73e203ea9050e36ec47ee77efba712cae5b4c37
marcsilva <marciliodevjava@gmail.com>
2024-01-18 17:29:56 -0300

    Aula 19

---
+++ aula19.py
@@ -0,0 +1,32 @@
+# Operadores aritmeticos
+
+zero = 0
+um = 1
+dois = 2
+tres = 3
+quatro = 4
+cinco = 5
+seis = 6
+sete = 7
+oito = 8
+nove = 9
+
+# Soma
+a = tres + dois
+print(f'Soma: {tres} + {dois} = {a}')
+
+# Subtração
+b = dois - um
+print(f'Subtração: {dois} - {um} = {b}')
+
+# Multiplicação
+c = sete * oito
+print('Multiplicação: {} x {} = {}'.format(sete, oito, c))
+
+# Divisão
+d = seis / tres
+print('Divisão: {} / {} = {}'.format(seis, tres, d))
+
+# Exponenciação
+e = quatro ** cinco
+print('Exponenciação: {} ** {} = {}'.format(quatro, cinco, e))
