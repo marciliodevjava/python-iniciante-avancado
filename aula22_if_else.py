c4a5702c3616dd19c81aff182b396dbd56888e7b
marcsilva <marciliodevjava@gmail.com>
2024-01-19 17:39:50 -0300

    aula 22

---
+++ aula22_if_else.py
@@ -0,0 +1,36 @@
+# If significa Se
+# Else significa Se não
+
+a = 1
+b = 2
+
+if a >= 0:
+    print(f'Valor maior que 0, valor: {a}')
+else:
+    print(f'Valor não é maior que zero')
+
+if b >= 0:
+    print(f'Valor maior que 0, valor: {b}')
+else:
+    print(f'Valor não é maior que zero')
+
+devo_continuar = True
+devo_parar = False
+
+if devo_continuar:
+    print('Verdadeiro')
+    if devo_parar:
+        print('Verdadeiro')
+    else:
+        print('Falso')
+
+pessoas = ['Paulo', 'Bruno', 'Camargo']
+# Convertendo todas as strings da lista para maiúsculas
+pessoas_upper = [p.upper() for p in pessoas]
+
+pessoa = str(input('Digite um nome: '))
+
+if pessoa.upper() in pessoas_upper:
+    print(f'A {pessoa}, está na lista')
+else:
+    print(f'A {pessoa}, não na lista')
