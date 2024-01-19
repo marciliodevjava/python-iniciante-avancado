a94f2374fcbdfd90813f0a577adb8ef9d48c07b5
marcsilva <marciliodevjava@gmail.com>
2024-01-19 13:11:44 -0300

    Update aula19

--- aula19.py
+++ aula19.py
@@ -11,6 +11,7 @@ sete = 7
 oito = 8
 nove = 9
 
+print('Operadores aritmeticos')
 # Soma
 a = tres + dois
 print(f'Soma: {tres} + {dois} = {a}')
@@ -30,3 +31,47 @@ print('Divisão: {} / {} = {}'.format(seis, tres, d))
 # Exponenciação
 e = quatro ** cinco
 print('Exponenciação: {} ** {} = {}'.format(quatro, cinco, e))
+print()
+
+# Operadores de Comparação
+print('Operadores de Comparação')
+# Igual '=='
+aa = cinco == dois
+print(f'Igual: {cinco} == {dois} = {aa}')
+
+# Diferente '!='
+bb = tres != dois
+print(f'Diferente: {tres} != {dois} = {bb}')
+
+# Maior
+cc = quatro > tres
+print('Maior: {} > {} = {}'.format(quatro, tres, cc))
+
+# Menor
+dd = um < cinco
+print('Menor: {} < {} = {}'.format(um, cinco, dd))
+
+# Maior igual
+ee = quatro >= tres
+print('Maior Igual: {} >= {} = {}'.format(quatro, tres, ee))
+
+# Menor Igual
+ff = um <= cinco
+print('Menor Igual: {} <= {} = {}'.format(um, cinco, ff))
+print()
+
+# Operadores Lógicos
+print('Operadores Lógicos')
+
+ab = True
+ac = False
+
+# E 'and'
+print(f"Verdadeiro {ab} and {ab} = {ab and ab}")
+
+# Ou 'or'
+print(f"Verdadeiro {ab} or {ac} = {ab or ac}")
+
+# Not
+print(f"Verdadeiro {ac} not = {not ac}")
+print(f"Falso {ab} not = {not ab}")
